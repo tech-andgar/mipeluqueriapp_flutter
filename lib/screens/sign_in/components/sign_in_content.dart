71cc2a5b92715037772764f452cc412d9835eb1c
Andres Garcia <andgar2010@gmail.com>
2020-08-23 17:27:30 -0500

    Added SignInContent (WIP)

---
+++ lib/screens/sign_in/components/sign_in_content.dart
@@ -0,0 +1,43 @@
+
+import 'package:MIPELUQUERIAPP/constants.dart';
+import 'package:MIPELUQUERIAPP/size_config.dart';
+import 'package:flutter/material.dart';
+
+class SignInContent extends StatelessWidget {
+  const SignInContent({
+    Key key,
+    this.text,
+    this.image,
+  }) : super(key: key);
+
+  final String text, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Spacer(),
+        Text(
+          "Mi PeluqueriApp",
+          style: TextStyle(
+            fontSize: getProportionteScreenWidth(36),
+            color: kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+        ),
+        Spacer(
+          flex: 2,
+        ),
+        Image.asset(
+          image,
+          height: getProportionteScreenHeight(265),
+          width: getProportionteScreenWidth(235),
+        )
+      ],
+    );
+  }
+}
