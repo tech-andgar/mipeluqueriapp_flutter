91d812ecc126105ad79cfed94cfb4c3b26e29f78
Andres Garcia <andgar2010@gmail.com>
2020-08-23 17:20:17 -0500

    Updated Routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
-import 'package:MIPELUQUERIAPP/screens/splash/splash_screen.dart';
 import 'package:flutter/widgets.dart';
+import 'package:MIPELUQUERIAPP/screens/sign_in/sign_in_screen.dart';
+import 'package:MIPELUQUERIAPP/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
 };
