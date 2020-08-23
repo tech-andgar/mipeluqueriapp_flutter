12278587afb2c0badc763b8588e0b71d59884063
Andres Garcia <andgar2010@gmail.com>
2020-08-23 03:27:20 -0500

    Added DefaultButtons and dots

---
+++ lib/components/button/default_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:MIPELUQUERIAPP/size_config.dart';
+import 'package:MIPELUQUERIAPP/constants.dart';
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({
+    Key key,
+    this.text,
+    this.press,
+  }) : super(key: key);
+  final String text;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      color: kPrimaryColor,
+      onPressed: () {},
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: getProportionteScreenWidth(18),
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
+import 'package:MIPELUQUERIAPP/components/button/default_button.dart';
+import 'package:MIPELUQUERIAPP/constants.dart';
 import 'package:MIPELUQUERIAPP/screens/splash/components/splash_content.dart';
+import 'package:MIPELUQUERIAPP/size_config.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatefulWidget {
@@ -8,6 +13,8 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  int currentPage = 0;
+
   List<Map<String, String>> splashData = [
     {"text": "Mi PeluqueriApp", 'image': 'assets/images/splash_1.jpg'},
     {"text": "Bienvienido al Mi PeluqueriApp, vamos pedirla!", 'image': 'assets/images/splash_1.jpg'},
@@ -24,6 +31,11 @@ class _BodyState extends State<Body> {
             Expanded(
               flex: 3,
               child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
                 itemCount: splashData.length,
                 itemBuilder: (context, index) => SplashContent(
                   image: splashData[index]['image'],
@@ -33,10 +45,51 @@ class _BodyState extends State<Body> {
             ),
             Expanded(
               flex: 2,
-              child: SizedBox(),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: getProportionteScreenWidth(20),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        splashData.length,
+                        (index) => buildDot(index: index),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 3,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: getProportionteScreenWidth(56),
+                      child: DefaultButton(
+                        text: 'Contiunar',
+                        press: () {},
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
             )
           ],
         ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: EdgeInsets.only(right: 5),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? kPrimaryColor : Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
       ),
     );
   }
