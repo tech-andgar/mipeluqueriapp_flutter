6ff8336df932f9b5c2be1b476badd9245ff6444d
Andres Garcia <andgar2010@gmail.com>
2020-08-23 21:31:41 -0500

    Updated Color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 const kTextTitleApp = 'Mi PeluqueriApp';
-const kPrimaryColor = Color(0xFFD29B88);
+// 1° OPTION
+// const kPrimaryColor = Color(0xFF2F5E66);
+// const kSecondaryColor = Color(0xFFD29B88);
+
+// 2° OPTION
+const kPrimaryColor = Color(0xFFBF6B04);
+const kSecondaryColor = Color(0xFF024873);
+// const kPrimaryColor = Color(0xFFA64F03);
+// const kPrimaryColor = Color(0xFFD9B29C);
+// const kPrimaryColor = Color(0xFF260101);
+
+// 3° OPTION
+// const kSecondaryColor = Color(0xFF024873);
+// const kSecondaryColor = Color(0xFF03658C);
+// const kPrimaryColor = Color(0xFF03A66A);
+// const kPrimaryColor = Color(0xFFBF8B78);
+// const kSecondaryColor = Color(0xFFA6453C);
+
 const kPrimaryLightColor = Color(0xFFFFECDF);
 const kPrimaryGradientColor =
     LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [Color(0xFFFA53E), Color(0xFFFF7643)]);
-const kSecondaryColor = Color(0xFF2F5E66);
 const kTextColor = Color(0xFF757575);
 
 const kAnimationDuration = Duration(milliseconds: 200);
