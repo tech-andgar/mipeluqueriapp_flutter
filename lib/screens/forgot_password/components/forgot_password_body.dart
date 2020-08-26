0bb8c0b182905684c3cf4bd3cc789eb4260d8b6b
Andres Garcia <andgar2010@gmail.com>
2020-08-25 22:20:41 -0500

    Updated complete screen forgot_password

--- lib/screens/forgot_password/components/forgot_password_body.dart
+++ lib/screens/forgot_password/components/forgot_password_body.dart
@@ -1,7 +1,11 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mi_peluqueriapp/components/button/default_button.dart';
+import 'package:mi_peluqueriapp/constants.dart';
 import 'package:mi_peluqueriapp/screens/sign_in/components/custom_surffix_icon.dart';
+import 'package:mi_peluqueriapp/screens/sign_in/components/form_error.dart';
+import 'package:mi_peluqueriapp/screens/sign_in/components/sign_in_no_account_text.dart';
 import 'package:mi_peluqueriapp/size_config.dart';
 
 class ForgotPasswordBody extends StatelessWidget {
@@ -12,78 +16,117 @@ class ForgotPasswordBody extends StatelessWidget {
     return Container(
       child: SizedBox(
         width: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(
-              height: getProportionteScreenWidth(14),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: getProportionteScreenWidth(20),
             ),
-            Text(
-              'Recuperar contraseña',
-              style: TextStyle(
-                fontSize: getProportionteScreenWidth(28),
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: getProportionteScreenWidth(14),
-            ),
-            Text(
-              'Por favor escribe tu email y\nse te enviará un link de recuperación a tu cuenta',
-              textAlign: TextAlign.center,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              // onSaved: (newValue) => _email = newValue,
-              // onChanged: (value) {
-              //   if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-              //     setState(() {
-              //       errors.remove(kEmailNullError);
-              //     });
-              //   } else if (EmailValidator.validate(value) && errors.contains(kInvalidEmailError)) {
-              //     setState(() {
-              //       errors.remove(kInvalidEmailError);
-              //     });
-              //   }
-              //   return null;
-              // },
-              // validator: (value) {
-              //   if (value.isEmpty && !errors.contains(kEmailNullError)) {
-              //     setState(() {
-              //       errors.add(kEmailNullError);
-              //     });
-              //   } else if (!EmailValidator.validate(value) && !errors.contains(kInvalidEmailError)) {
-              //     setState(() {
-              //       errors.add(kInvalidEmailError);
-              //     });
-              //   }
-              //   return null;
-              // },
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Ingresa su email',
-                suffixIcon: CustomSurffixIcon(
-                  icon: FontAwesomeIcons.envelope,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.04,
                 ),
-                // floatingLabelBehavior: FloatingLabelBehavior.always,
-                // contentPadding: EdgeInsets.symmetric(horizontal: 42, vertical: 20),
-                // enabledBorder: OutlineInputBorder(
-                //   borderRadius: BorderRadius.circular(28),
-                //   borderSide: BorderSide(color: kTextColor),
-                //   gapPadding: 10,
-                // ),
-                // focusedBorder: OutlineInputBorder(
-                //   borderRadius: BorderRadius.circular(28),
-                //   borderSide: BorderSide(color: kTextColor),
-                //   gapPadding: 10,
-                // ),
+                Text(
+                  'Recuperar contraseña',
+                  style: TextStyle(
+                    fontSize: getProportionteScreenWidth(28),
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: getProportionteScreenWidth(14),
+                ),
+                Text(
+                  'Por favor escribe tu email y\nse te enviará un link de recuperación a tu cuenta',
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.1,
+                ),
+                ForgotPasswordForm(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPasswordForm extends StatefulWidget {
+  ForgotPasswordForm({Key key}) : super(key: key);
+
+  @override
+  _ForgotPasswordFormState createState() => _ForgotPasswordFormState();
+}
+
+class _ForgotPasswordFormState extends State<ForgotPasswordForm> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> errors = [];
+  String _email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            onSaved: (newValue) => _email = newValue,
+            onChanged: (value) {
+              if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+                setState(() {
+                  errors.remove(kEmailNullError);
+                });
+              } else if (EmailValidator.validate(value) && errors.contains(kInvalidEmailError)) {
+                setState(() {
+                  errors.remove(kInvalidEmailError);
+                });
+              }
+              return null;
+            },
+            validator: (value) {
+              if (value.isEmpty && !errors.contains(kEmailNullError)) {
+                setState(() {
+                  errors.add(kEmailNullError);
+                });
+              } else if (!EmailValidator.validate(value) && !errors.contains(kInvalidEmailError)) {
+                setState(() {
+                  errors.add(kInvalidEmailError);
+                });
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'Ingresa su email',
+              suffixIcon: CustomSurffixIcon(
+                icon: FontAwesomeIcons.envelope,
               ),
             ),
-            DefaultButton(
-              text: 'Enviar',
-            ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: getProportionteScreenHeight(30),
+          ),
+          FormErrors(errors: errors),
+          SizedBox(
+            height: SizeConfig.screenHeight * 0.1,
+          ),
+          DefaultButton(
+            text: 'Enviar',
+            press: () {
+              if (_formKey.currentState.validate()) {
+                // Do what you want to do
+              }
+            },
+          ),
+          SizedBox(
+            height: SizeConfig.screenHeight * 0.1,
+          ),
+          NoAccountText(),
+        ],
       ),
     );
   }
