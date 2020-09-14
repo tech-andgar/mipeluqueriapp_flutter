cfd99be80d7159700a8cd176be07e2d4344e4ca8
Andres Garcia <andgar2010@gmail.com>
2020-09-14 01:56:30 -0500

    Added Categories

---
+++ lib/screens/home/components/home_catergories.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mi_peluqueriapp/screens/home/components/home_category_card.dart';
+import 'package:mi_peluqueriapp/size_config.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>> categories = [
+      {"icon": FontAwesomeIcons.flask, "text": "Flash Bot"},
+      {"icon": FontAwesomeIcons.accusoft, "text": "Tecnologia"},
+      {"icon": FontAwesomeIcons.appleAlt, "text": "Super mercado"},
+      {"icon": FontAwesomeIcons.hamburger, "text": "Comidas Rapidas"},
+      {"icon": FontAwesomeIcons.caravan, "text": "Servicios"},
+      {"icon": FontAwesomeIcons.handPeace, "text": "Aseo"},
+      {"icon": FontAwesomeIcons.untappd, "text": "Transporte"},
+      {"icon": FontAwesomeIcons.notesMedical, "text": "Salud"},
+    ];
+
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: getProportionteScreenWidth(12)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ...List.generate(
+              categories.length,
+              (index) => CategoryCard(
+                icon: categories[index]["icon"],
+                text: categories[index]["text"],
+                press: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
