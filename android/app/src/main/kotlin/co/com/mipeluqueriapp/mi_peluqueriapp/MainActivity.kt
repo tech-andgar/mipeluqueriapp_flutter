88318e5c3be1dec738079ce1fa957ad29be90ba5
Andres Garcia <andgar2010@gmail.com>
2020-09-15 19:04:07 -0500

    Remove lines of main KT

--- android/app/src/main/kotlin/co/com/mipeluqueriapp/mi_peluqueriapp/MainActivity.kt
+++ android/app/src/main/kotlin/co/com/mipeluqueriapp/mi_peluqueriapp/MainActivity.kt
@@ -2,21 +2,6 @@ package co.com.mipeluqueriapp.client
 
 import io.flutter.embedding.android.FlutterActivity
 
-import io.flutter.app.FlutterApplication;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+class MainActivity: FlutterActivity() {
 
-class MainActivity: FlutterActivity() implements PluginRegistrantCallback {
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    FlutterFirebaseMessagingService.setPluginRegistrant(this);
-  }
-
-  @Override
-  public void registerWith(PluginRegistry registry) {
-    GeneratedPluginRegistrant.registerWith(registry);
-  }
 }
