3520b4a42daab0f35de62d904017878871b03d86
Andres Garcia <andgar2010@gmail.com>
2020-09-18 04:13:55 -0500

    Desactive some imports

--- lib/screens/home/components/home_body.dart
+++ lib/screens/home/components/home_body.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:mi_peluqueriapp/constants.dart';
-import 'package:mi_peluqueriapp/data/models/Product_model.dart';
-import 'package:mi_peluqueriapp/components/product_card.dart';
-import 'package:mi_peluqueriapp/screens/home/components/home_popular_products.dart';
-import 'package:mi_peluqueriapp/screens/home/components/home_section_catergories.dart';
+import 'package:mi_peluqueriapp/screens/home/components/home_categories_services.dart';
 import 'package:mi_peluqueriapp/screens/home/components/home_discount_banner.dart';
-import 'package:mi_peluqueriapp/screens/home/components/home_header.dart';
-import 'package:mi_peluqueriapp/screens/home/components/home_section_title.dart';
-import 'package:mi_peluqueriapp/screens/home/components/home_special_offers.dart';
 import 'package:mi_peluqueriapp/size_config.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:mi_peluqueriapp/constants.dart';
+// import 'package:mi_peluqueriapp/data/models/Product_model.dart';
+// import 'package:mi_peluqueriapp/components/product_card.dart';
+// import 'package:mi_peluqueriapp/screens/home/components/home_popular_products.dart';
+// import 'package:mi_peluqueriapp/screens/home/components/home_section_catergories.dart';
+// import 'package:mi_peluqueriapp/screens/home/components/home_header.dart';
+// import 'package:mi_peluqueriapp/screens/home/components/home_section_title.dart';
+// import 'package:mi_peluqueriapp/screens/home/components/home_special_offers.dart';
 
 class HomeBody extends StatefulWidget {
   HomeBody({Key key}) : super(key: key);
