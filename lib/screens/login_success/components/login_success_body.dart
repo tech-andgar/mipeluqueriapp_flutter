11f8059752a0c4c2cf3762200cc284ab491c59ea
Andres Garcia <andgar2010@gmail.com>
2020-09-18 10:13:30 -0500

    Change LoginSuccessBody

--- lib/screens/login_success/components/login_success_body.dart
+++ lib/screens/login_success/components/login_success_body.dart
@@ -1,16 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:mi_peluqueriapp/components/button/default_button.dart';
-import 'package:mi_peluqueriapp/constants.dart';
+import 'package:lottie/lottie.dart';
 import 'package:mi_peluqueriapp/screens/home/home_screen.dart';
-import 'package:mi_peluqueriapp/screens/sign_in/sign_in_screen.dart';
-import 'package:mi_peluqueriapp/services/auth.dart';
+import 'package:mi_peluqueriapp/size_config.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:mi_peluqueriapp/components/button/default_button.dart';
+// import 'package:mi_peluqueriapp/constants.dart';
+// import 'package:mi_peluqueriapp/screens/sign_in/sign_in_screen.dart';
+// import 'package:mi_peluqueriapp/services/auth.dart';
 // import 'package:mi_peluqueriapp/screens/sign_in/services/auth.dart';
 // import 'package:mi_peluqueriapp/screens/sign_in/sign_in_screen.dart';
-import 'package:mi_peluqueriapp/size_config.dart';
 
-class LoginSuccessBody extends StatelessWidget {
-  // const LoginSuccessBody({Key key}) : super(key: key);
+class LoginSuccessBody extends StatefulWidget {
+  // const LoginSuccessBody({Key key}) : super(key: key);}
+
+  @override
+  _LoginSuccessBodyState createState() => _LoginSuccessBodyState();
+}
+
+class _LoginSuccessBodyState extends State<LoginSuccessBody> {
+  Timer _timer;
+  _LoginSuccessBodyState() {
+    _timer = Timer(const Duration(milliseconds: 3500), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+  }
+// var timer = Timer(Duration(seconds: 2), () => Navigator.pushNamed(context, HomeScreen.routeName));
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,31 +43,32 @@ class LoginSuccessBody extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: SizeConfig.screenHeight * 0.04),
-          Icon(
-            FontAwesomeIcons.check,
-            color: kPrimaryColor,
-            size: SizeConfig.screenHeight * 0.4, //%40
-          ),
-          SizedBox(height: SizeConfig.screenHeight * 0.08),
-          Text(
-            "Logueo Exitoso!",
-            style: TextStyle(
-              fontSize: getProportionteScreenWidth(30),
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          Spacer(),
-          SizedBox(
-            width: SizeConfig.screenWidth * 0.6,
-            height: getProportionteScreenWidth(50),
-            child: DefaultButton(
-              text: "Ir al Inicio",
-              press: () {
-                Navigator.pushNamed(context, HomeScreen.routeName);
-              },
-            ),
-          ),
+          Lottie.asset('assets/images/successAuth.json'),
+          // Icon(
+          //   FontAwesomeIcons.check,
+          //   color: kPrimaryColor,
+          //   size: SizeConfig.screenHeight * 0.4, //%40
+          // ),
+          // SizedBox(height: SizeConfig.screenHeight * 0.08),
+          // Text(
+          //   "Logueo Exitoso!",
+          //   style: TextStyle(
+          //     fontSize: getProportionteScreenWidth(30),
+          //     fontWeight: FontWeight.bold,
+          //     color: Colors.black,
+          //   ),
+          // ),
+          // Spacer(),
+          // SizedBox(
+          //   width: SizeConfig.screenWidth * 0.6,
+          //   height: getProportionteScreenWidth(50),
+          //   child: DefaultButton(
+          //     text: "Ir al Inicio",
+          //     press: () {
+          //       Navigator.pushNamed(context, HomeScreen.routeName);
+          //     },
+          //   ),
+          // ),
           // DefaultButton(
           //   text: "Sign out",
           //   press: () {
